40df504ccd78eedaa1d7d3b8eb4d64869422acba
mtall587 <matthewtallman20@gmail.com>
2024-06-11 16:21:48 -0600

    changed

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 import os
 from azure.storage.blob import BlobServiceClient
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 connection_string = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
 blob_service_client = BlobServiceClient.from_connection_string(connection_string)
@@ -18,10 +18,14 @@ def portfolio():
 @app.route('/get_images')
 def get_images():
     category = request.args.get('category')
-    container_client = blob_service_client.get_container_client('images')
-    blob_list = container_client.list_blobs(name_starts_with=category)
-    image_urls = [blob.url for blob in blob_list]
-    return jsonify(image_urls)
+    container_client = blob_service_client.get_container_client(category)
+    blob_list = container_client.list_blobs()
+    images = [f"https://{blob_service_client.account_name}.blob.core.windows.net/{category}/{blob.name}" for blob in blob_list]
+    return jsonify(images)
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
